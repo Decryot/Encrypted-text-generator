a30e47885a8d2f0dfcc7b5daf99575dd7c51f6a7
Dmitri <56200546+Decryot@users.noreply.github.com>
2020-07-06 12:28:30 -0400

    word encryption in C#

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,20 +6,50 @@ namespace TextEncrypter
     {
         static string[] letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"};
 
-        public static string generateString(int wordLength) {
+        public static string generateString(string word, int percent) {
+
             string encrypted = "";
 
-            for (int letter = 0; letter < wordLength; letter++) {
-                Random randomLetter = new Random();
-                int letterPlace = randomLetter.Next(0, letters.Length);
-                string newLetter = letters[letterPlace];
+            char[] lettersInWord = word.ToCharArray();
 
+            char[] savedLetters = new char[lettersInWord.Length];
+            bool[] letterPlaces = new bool[lettersInWord.Length];
+
+            // saves specific letters
+            for (int i = 0; i < lettersInWord.Length; i++) {
+                Random perc = new Random();
+                int percentage = perc.Next(1, 100);
+                if (percentage > percent) {
+                    savedLetters[i] = lettersInWord[i];
+                    letterPlaces[i] = true;
+                }
+            }
+
+            for (int i = 0; i < lettersInWord.Length; i++)
+            {
                 Random randomCap = new Random();
                 int cap = randomCap.Next(0, 2);
 
-                if (cap == 0) encrypted += newLetter.ToUpper();
-                else encrypted += newLetter;
+                Random randomLetter = new Random();
+                int letterID = randomLetter.Next(0, letters.Length);
+
+                // places saved letters back in their positions
+                if (letterPlaces[i] == true) {
+                    string letter = lettersInWord[i].ToString();
+
+                    if (cap == 0) encrypted += letter.ToUpper();
+                    else encrypted += letter;
+                }
+                // replaces the non saved letters
+                else {
+                    string letter = letters[letterID];
+
+                    if (cap == 0) encrypted += letter.ToUpper();
+                    else encrypted += letter;
+                }
             }
+
+            
             return encrypted;
         }
     }
@@ -29,8 +59,9 @@ namespace TextEncrypter
         static string encrypted;
         public static void Main(string[] args)
         {
-            encrypted = Letters.generateString(50);
-            Console.WriteLine(encrypted);
+            // the higher the percentage, the more ecrypted the word becomes
+            encrypted = Letters.generateString("Institution", 40);
+            Console.WriteLine(encrypted + " encrypted");
         }
     }
 }
